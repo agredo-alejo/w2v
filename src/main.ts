9177df1dd4638d5b5ee6e84b7c96cd4bd1fae94a
agredo-alejo <agredoalejo@gmail.com>
2022-08-04 17:14:40 -0500

    aaa

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { colores } from "./colors.json";
+// import * as data from "./colors.json"
+import {  add, create, range} from "@apjs/tensor";
 
 
-console.log(colores[0]);
+// add
+let a =range(0,6,1)
+a.reshape([6,1])
+
+let b =create([1,2,3,4], [2,2])
+console.table(a.data);
+
+
+colores[0].value
+let azul = create(colores[0].value)
+let rojo = create(colores[1].value)
+
+let morado = add(azul, rojo)
+console.table(morado.data)
+
+
+// a = name()
